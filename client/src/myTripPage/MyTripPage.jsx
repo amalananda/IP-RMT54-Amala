edbabeb3d6f88dc368c31fe77ef9a64883afcddd
amala <amalananda332@gmail.com>
2024-11-01 01:01:54 +0700

    feat: create myTripPage

---
+++ client/src/myTripPage/MyTripPage.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function MyTrips() {
+  const [trips, setTrips] = useState([])
+
+  const fetchTrips = async () => {
+    try {
+      const accessToken = localStorage.getItem("access_token")
+      const response = await axios.get("http://13.215.50.60/trips/my-trips", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      setTrips(response.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchTrips()
+  }, [])
+
+  return (
+    <div>
+      <h2>My Trips</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>{trip.title}</li> // Tampilkan judul perjalanan
+        ))}
+      </ul>
+    </div>
+  )
+}
